d33579990dd8b2dce3446d4e8ed87f60eb7ad83f
Ariandy Noviar <ariandy.noviar@modalrakyat.com>
2020-01-04 21:25:15 +0700

    Fix balance mechanism

--- blockchain.py
+++ blockchain.py
@@ -6,9 +6,23 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Ariandy'
+participants = {'Ariandy'}
 
 def hash_block(block):
 	return 'regulus'.join([str(block[key]) for key in block])
+
+def get_balance(participant):
+	tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+	amount_sent = 0
+	for tx in tx_sender:
+		if len(tx) > 0:
+			amount_sent += tx[0]
+	tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+	amount_received = 0
+	for tx in tx_recipient:
+		if len(tx) > 0:
+			amount_received += tx[0]
+	return amount_received - amount_sent
 
 def get_last_blockchain_value():
 	if len(blockchain) < 1:
@@ -22,6 +36,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 		'amount': amount
 	}
 	open_transactions.append(transaction)
+	participants.add(sender)
+	participants.add(recipient)
 
 def mine_block():
 	hashed_block = hash_block(get_last_blockchain_value())
@@ -32,6 +48,7 @@ def mine_block():
 		'transactions': open_transactions
 	}
 	blockchain.append(block)
+	return True
 
 def get_transaction_value():
 	tx_recipient = input('Enter the recipient of the transaction: ')
@@ -61,7 +78,8 @@ while waiting_for_input:
 	print('1: Add new transaction value')
 	print('2: Mine new block')
 	print('3: Output the blockchain blocks')
-	print('4: Chain Manipulation')
+	print('4: Participants list')
+	print('h: Chain Manipulation')
 	print('q: Quit')
 	user_choice = get_user_choice()
 	if user_choice == '1':
@@ -70,9 +88,12 @@ while waiting_for_input:
 		add_transaction(recipient, amount=amount)
 		print(open_transactions)
 	elif user_choice == '2':
-		mine_block()
+		if mine_block():
+			open_transactions = []
 	elif user_choice == '3':
 		print_blockchain_elements()
+	elif user_choice == '4':
+		print(participants)
 	elif user_choice == 'h':
 		if len(blockchain) >= 1:
 			blockchain[0] = {
@@ -92,6 +113,7 @@ while waiting_for_input:
 		print_blockchain_elements()
 		print('Invalid Blockchain')
 		break
+	print(get_balance('Ariandy'))
 else:
 	print('User Left')
 
